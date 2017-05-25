01cf99cfa20bbc05b17263f9ff9de563eebc992e
Jacquelin Huang <Jacquelin@D-173-250-155-198.dhcp4.washington.edu>
2017-05-25 12:56:48 -0700

    added ui features

--- server.R
+++ server.R
@@ -1,1 +1,26 @@
 #server.R
+library(shiny)
+library(plotly)
+
+data <- read.csv(file='data/xAPI-Edu-Data.csv', stringsAsFactors = FALSE)
+
+shinyServer(function(input, output) {
+  output$main.text <- renderText({
+    "The dataset is an educational data set which is collected from learning management system (LMS) called Kalboard 360. Kalboard 360 is a multi-agent LMS, which has been designed to facilitate learning through the use of leading-edge technology. The data is collected using a learner activity tracker tool, which called experience API (xAPI). The xAPI is a component of the training and learning architecture (TLA) that enables to monitor learning progress and learner’s actions like reading an article or watching a training video. There are 16 attributes in this data set. The students are divided into three intervals(numerical) based on their total grade or mark; Low-Level indicates grade 0 to 69, Middle-Level means 70 to 89, and High-Level represents the score of 90 to 100. 
+    There are multiple audiences that would be interested in using this dataset. 
+    Parents, teachers, students, and anyone who is involved in education will be interested in using this dataset. 
+    our project, we will be focusing on parents. A student’s performance in school usually concerns the parents, and organizing the dataset can provide visual insight on how certain factors affect students’ grades.
+    Here are some questions that our project will answer for the audience. 
+
+    1. Students from which nation tend to achieve highest average academic performance? 
+    2. Do males tend to achieve better academic performance than females? 
+    3. What kinds of academic behavior leads to enhancing grade of students?"
+  })
+  output$scatter <- renderPlotly({
+    
+  })
+  output$table <- renderPlotly({
+    
+  })
+  
+})
--- ui.R
+++ ui.R
@@ -1,1 +1,29 @@
 #ui.R
+library(shiny)
+library(plotly)
+library(shinythemes)
+#install.packages("shinythemes")
+
+
+shinyUI(fluidPage( theme=shinytheme("superhero"),
+  titlePanel("Final Project - Team Run"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      conditionalPanel(condition='input.tabselected == 1', helpText("Overview Panel")),
+      conditionalPanel(condition='input.tabselected == 2', helpText("Scatterplot Panel"),
+                       selectInput("scatter.options","Choose an option", choices = c("Option 1", "Option 2", "Option 3"))),
+      conditionalPanel(condition='input.tabselected == 3', helpText("Table Panel"),
+                       selectInput("table.options","Choose an option", choices = c("Option 1", "Option 2", "Option 3")))
+    ),
+    mainPanel(
+      tabsetPanel(
+        id="tabselected",
+        tabPanel("Overview", value=1,textOutput("main.text")),
+        tabPanel("Graph", value=2, plotlyOutput("scatter")),
+        tabPanel("Table", value=3,plotlyOutput("table"))
+      )
+    )
+  )
+  
+))
