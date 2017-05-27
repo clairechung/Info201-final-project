6a1646b603e0c92ff780278490fd142a2407cc97
clairechung <clairechung27@gmail.com>
2017-05-26 21:44:50 -0700

    Local changes

--- server.R
+++ server.R
@@ -1,6 +1,7 @@
 #server.R
 library(shiny)
 library(plotly)
+library(dplyr)
 
 data <- read.csv(file='data/xAPI-Edu-Data.csv', stringsAsFactors = FALSE)
 
@@ -20,6 +21,40 @@ shinyServer(function(input, output) {
     
   })
   output$table <- renderPlotly({
+    # group data by its nationality and class(test scores)
+    # sum up value of Raised hand, Visited resources, Viewing announcements, Discussion groups
+    # to find how much students participated in class in each nation (also what level did they get from the test)
+    group.data <- group_by(data, NationalITy, Class) %>% 
+      summarise(mean.activity = mean(raisedhands+ VisITedResources+ AnnouncementsView+ Discussion)) %>%
+      arrange(Class, NationalITy)
+    
+    # 3 indicates high level (score of 90 - 100), 
+    # 2 indicates middle level (70 - 89), 1 indicates low level (0-69)
+    group.data$Class <- gsub('H', 3, group.data$Class)
+    group.data$Class <- gsub('M', 2, group.data$Class)
+    group.data$Class <- gsub('L', 1, group.data$Class)
+    
+    if(input$table.options == "High Performance") {
+      group.data <- filter(group.data, Class == 3)
+    } else if (input$table.options == "Middle Performance") {
+      group.data <- filter(group.data, Class == 2)
+    } else if (input$table.options == "Low Performance") {
+      group.data <- filter(group.data, Class == 1)
+    }
+    
+    m <- list(
+      l = 80,
+      r = 50,
+      b = 80,
+      t = 20,
+      pad = 4
+    )
+    
+    # makes bar graph; xaxis is nationality of students and yaxis is active participation in class
+    plot_ly(group.data, x = ~group.data$NationalITy, y = ~group.data$mean.activity, 
+            type = 'bar', color = group.data$Class) %>%
+      layout(yaxis = list(title = 'Level of Participation'), xaxis = list(title = 'Nationality'), barmode = 'stack', margin = m, height = 500)
+    
     
   })
   
