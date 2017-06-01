a0cb236ed723db6bf5fefa704eb434cb77511a33
MyungJin Eun <eunmyun@uw.edu>
2017-05-31 20:32:08 -0700

    Published and added url to readme.md

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Info201-final-project
 
 ## Project Link
-[Hosted webpage]("google.com")
+[Hosted webpage]("https://lucyeun.shinyapps.io/Info201-final-project/")
 
 ## Overview
 We will be working with Students’ Academic Performance Dataset, obtained from kaggle.com. The dataset is an educational data set which is collected from learning management system (LMS) called Kalboard 360. Kalboard 360 is a multi-agent LMS, which has been designed to facilitate learning through the use of leading-edge technology. The data is collected using a learner activity tracker tool, which called experience API (xAPI). The xAPI is a component of the training and learning architecture (TLA) that enables to monitor learning progress and learner’s actions like reading an article or watching a training video. There are multiple audiences that would be interested in using this dataset.
--- ui.R
+++ ui.R
@@ -140,23 +140,19 @@ shinyUI(
                           )
                       )
                       ),
-             tabPanel("Conclusion",
-                      titlePanel("Answering the Questions"),
+             tabPanel("Report",
                       mainPanel(
-                        tags$br(),
                         tags$div(
                           tags$h3("What factors affect academic performance?"),
                           tags$p("From our scatter plot tab we are able to filter through four different options that may affect student performance. 
-                                 As we look at each feature it is apparent that lower instances of these factors, (raising hands, viewing annoucements, joining discussion groups) 
-                                 leads to a higher density of students plotted along the lower tiered performance. 
-                                 For example, selecting the default option of raised hands on the x-axis you can see a clear visual of the students who raised their hands less are populating the performance rating of 1.
-                                 It is less clear that higher frequencies of these tasks directly translate to higher tiered performance level, but applying extra filters can show more prominent evidence.
-                                 We also claim that correlation does not mean causation and there could be multiple factors that affect performance."),
+                                 As we look at each feature, it is apparent that lower instances of these factors, (raising hands, viewing annoucements, joining discussion groups) 
+                                 lead to a higher density of students plotted along the lower tiered performance. 
+                                 For example, selecting the default option of raised hands on the x-axis you can see a clear relationship: students who raised their hands less are populating the performance rating of 1.
+                                 It is less clear that higher frequencies of these tasks directly translate to higher tiered performance level, but applying extra filters may show more prominent evidence."),
                           tags$h3("Does a specific gender tend to be more influenced by the academic behaviors?"),
-                          tags$p("For females, there is a more direct trend that we are looking for in the visted resource factor. 
+                          tags$p("For females, there is a more direct trend for the visted resource factor. 
                                  We see that lower frequencies of doing this task shows more students in the lower performance scoring and higher frequecies of visiting resources shows a higher chance of scoring a performance level of 3.
-                                 For males, this trend is more prominent while showing the data results from raised hands. We also can see that in both female and males that lower participation in these factors can lead to a higher chance of placement within the lower tiered performance level of 1.
-                                 It is not clear though whether the maximum amount of performing these factors leads to the highest grades."),
+                                 For males, this trend is more prominent while showing the data results from raised hands. We also can see that in both female and males that lower participation in these factors can lead to a higher chance of placement within the lower tiered performance level of 1."),
                           tags$h3("Students from which nation tend to achieve highest average academic performance?"),
                           tags$p("As mentioned before, the lowest average performance data we found was from Saudi Arabia (participated 141 time),
                                  and the highest average academic performance was from the USA (participated 224 times)"),
@@ -164,10 +160,15 @@ shinyUI(
                           tags$p("To recieve a low performance grading (scoring of 1) participation levels range from 29-141 times participated.
                                  To recieve a mid-tier perfomance grading (scoring of 2) participation levels range from 126-266 times participated.
                                  To recieve a high-tier performance grading (scoring of 3) participation levels range from 223-330
-                                 It it evident that there is a higher frequency of participation required to recieve the highest performance grading level, and the reverse is true as well 
+                                 It it possible that there is a higher frequency of participation required to recieve the highest performance grading level, and the reverse is possible as well 
                                  where lower amounts of participation leads to a higher chance of recieving a lower grade.")
-                          )
-                        
+                          ),
+                        tags$div(
+                          tags$h3("Conclusion"),
+                          tags$p("By analyzing the plots , we were able to identify that there exists clear positive relationship between students' academic performance and 
+                          their participation. However, correlation does not necessarily mean causation, it is possible that there are some other factors that may
+                          be affecting the relationship. In order to prove the causation of the correlationi, more research will have to be done in future.")
+                        )
                           ))
                           )
                           )
